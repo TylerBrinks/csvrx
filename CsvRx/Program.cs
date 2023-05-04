815e5474ecd1ea2b63a2004b06f53cfda634d763
Tyler Brinks <tylerbrinks@hotmail.com>
2023-05-04 10:11:39 -0600

    Console help

--- CsvRx/Program.cs
+++ CsvRx/Program.cs
@@ -1,33 +1,75 @@
 ﻿using Spectre.Console;
+using SqlParser;
 
 var context = new CsvRx.Execution.ExecutionContext();
 // ReSharper disable StringLiteralTypo
 context.RegisterCsv("aggregate_test_100", @"C:\Users\tyler\source\repos\sink\sqldatafusion\testing\data\csv\aggregate_test_100.csv");
 //var results = context.ExecuteSql("SELECT c1, MAX(c3) FROM aggregate_test_100 GROUP BY c1"); //WHERE c11 > .2 AND c11 < 0.9 
 //var results = context.ExecuteSql("SELECT * FROM aggregate_test_100");
-
 //var results = context.ExecuteSql("SELECT avg(c3) FROM aggregate_test_100 group by c1");
-var results = context.ExecuteSql("SELECT c1, c3 FROM aggregate_test_100 order by c1, c3");
-//SELECT c1, c3 FROM aggregate_test_100 order by c1, c3 desc
-Console.WriteLine();
+//var results = context.ExecuteSql("SELECT c1, c3 FROM aggregate_test_100 order by c1, c3");
 
-Table? table = null;
+//var sql = "SELECT distinct s s s c1 as abc FROM aggregate_test_1003";
+var sql = """
+    SELECT distinct c1 as abc
+    FROM aggregate_test_1003
+    """;
 
-await foreach (var batch in results)
+try
 {
-    if(table == null)
+    var results = context.ExecuteSql(sql);
+
+    Console.WriteLine();
+
+    Table? table = null;
+
+    await foreach (var batch in results)
     {
-        table = new Table();
-        foreach (var field in batch.Schema.Fields)
+        if (table == null)
         {
-            table.AddColumn(field.Name);
+            table = new Table();
+            foreach (var field in batch.Schema.Fields)
+            {
+                table.AddColumn(field.Name);
+            }
+        }
+
+        for (var i = 0; i < batch.RowCount; i++)
+        {
+            table.AddRow(batch.Results.Select(value => value.Values[i]?.ToString() ?? "").ToArray());
         }
     }
 
-    for (var i = 0; i < batch.RowCount; i++)
-    {
-        table.AddRow(batch.Results.Select(value => value.Values[i]?.ToString() ?? "").ToArray());
-    }
+    AnsiConsole.Write(table);
+}
+catch (ParserException pe)
+{
+    NotifyInvalidSyntax(sql, pe.Message, Convert.ToUInt32(pe.Line), Convert.ToUInt32(pe.Column));
+}
+catch (TokenizeException te)
+{
+    NotifyInvalidSyntax(sql, te.Message, Convert.ToUInt32(te.Line), Convert.ToUInt32(te.Column));
+}
+catch (Exception ex)
+{
+    throw ex;
 }
 
-AnsiConsole.Write(table);
+static void NotifyInvalidSyntax(string query, string message, uint line, uint column)
+{
+    AnsiConsole.MarkupLine("[red]Invalid SQL Statement[/]");
+    AnsiConsole.MarkupLine($"[red]{message}[/]");
+    AnsiConsole.WriteLine();
+    var lines = query.Split(Environment.NewLine).ToList();
+    var pointer = new string('-', (int)column - 1);
+
+    var index = (int)line;
+
+    lines.Insert(index, $"{pointer}^");
+
+    for (var i = 0; i < lines.Count; i++)
+    {
+        var color = i == index ? "yellow" : "teal";
+        AnsiConsole.MarkupLine($"[{color}]{lines[i]}[/]");
+    }
+}
